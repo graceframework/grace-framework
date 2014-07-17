f6586ee36783b1c7df2439f703c0d6e4d8e4542d
Jeff Scott Brown <jbrown@gopivotal.com>
2014-07-17 16:58:06 -0500

    move trait injection related classes to grails.compiler.traits

--- grails-web-taglib/src/main/groovy/grails/compiler/traits/TagLibraryTraitInjector.groovy
+++ grails-web-taglib/src/main/groovy/grails/compiler/traits/TagLibraryTraitInjector.groovy
@@ -13,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.grails.compiler.web.taglib
+package grails.compiler.traits
 
 import grails.artefact.TagLibrary
 
 import java.util.regex.Pattern
 
-import org.grails.compiler.injection.TraitInjector
 import org.grails.io.support.GrailsResourceUtils
 
 /**
