4c88d9ef33d8c6b14a257e273be532c5157d0334
Michael Yan <rainboyan@163.com>
2022-05-22 23:15:55 +0800

    remove extra parentheses

--- grails-core/src/main/groovy/grails/plugins/DefaultGrailsPluginManager.java
+++ grails-core/src/main/groovy/grails/plugins/DefaultGrailsPluginManager.java
@@ -246,7 +246,7 @@ public class DefaultGrailsPluginManager extends AbstractGrailsPluginManager {
         pluginList = sortPlugins(pluginList);
         initializePlugins();
         initialised = true;
-        LOG.info(String.format("Total %d plugins loaded successfully, take in %dms.)", pluginList.size(), (System.currentTimeMillis() - time)));
+        LOG.info(String.format("Total %d plugins loaded successfully, take in %dms.", pluginList.size(), (System.currentTimeMillis() - time)));
     }
 
     protected List<GrailsPlugin> sortPlugins(List<GrailsPlugin> toSort) {
