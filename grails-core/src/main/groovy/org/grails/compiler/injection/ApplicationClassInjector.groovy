6517eabb6eb7f7c1184174f68c71c5401149baab
Michael Yan <rainboyan@163.com>
2022-05-30 15:46:11 +0800

    Update copyright year and author

--- grails-core/src/main/groovy/org/grails/compiler/injection/ApplicationClassInjector.groovy
+++ grails-core/src/main/groovy/org/grails/compiler/injection/ApplicationClassInjector.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,10 +44,12 @@ import org.grails.io.support.UrlResource
 import org.springframework.util.ClassUtils
 import static org.codehaus.groovy.ast.tools.GeneralUtils.*
 import java.lang.reflect.Modifier
+
 /**
  * Injector for the 'Application' class
  *
  * @author Graeme Rocher
+ * @author Michael Yan
  * @since 3.0
  */
 @CompileStatic
