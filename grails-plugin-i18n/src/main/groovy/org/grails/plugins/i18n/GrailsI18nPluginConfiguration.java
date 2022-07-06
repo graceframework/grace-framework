ae433b4a2dd6efbe76f5fd7f87be2c31a0ed3c6b
Michael Yan <rain@rainboyan.com>
2022-07-07 01:02:24 +0800

    Using Spring Boot Auto Configure, no Micronaut Spring
    Rename DefaultLocaleConfiguration to GrailsI18nPluginConfiguration

--- grails-plugin-i18n/src/main/groovy/org/grails/plugins/i18n/GrailsI18nPluginConfiguration.java
+++ grails-plugin-i18n/src/main/groovy/org/grails/plugins/i18n/GrailsI18nPluginConfiguration.java
@@ -16,21 +16,33 @@
 package org.grails.plugins.i18n;
 
 import org.grails.web.i18n.ParamsAwareLocaleChangeInterceptor;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.LocaleResolver;
 
 /**
- * Default locale configuration.
+ * Grails locale configuration.
  *
  * @author Michael Yan
- * @since 5.1
+ * @since 2022.0.0
  */
-@Configuration
-public class DefaultLocaleConfiguration {
+@Configuration(proxyBeanMethods = false)
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE + 8)
+public class GrailsI18nPluginConfiguration {
+    private final WebProperties webProperties;
+
+    public GrailsI18nPluginConfiguration(WebProperties webProperties) {
+        this.webProperties = webProperties;
+    }
 
     @Bean
     @ConditionalOnMissingBean
@@ -41,8 +53,15 @@ public class DefaultLocaleConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name = DispatcherServlet.LOCALE_RESOLVER_BEAN_NAME)
     public LocaleResolver localeResolver() {
+        if (this.webProperties.getLocaleResolver() == WebProperties.LocaleResolver.FIXED) {
+            return new FixedLocaleResolver(this.webProperties.getLocale());
+        } else if (this.webProperties.getLocaleResolver() == WebProperties.LocaleResolver.ACCEPT_HEADER) {
+            AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+            localeResolver.setDefaultLocale(this.webProperties.getLocale());
+            return localeResolver;
+        }
         return new SessionLocaleResolver();
     }
 }
