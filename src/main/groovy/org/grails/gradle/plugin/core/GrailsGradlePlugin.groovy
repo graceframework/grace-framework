80c5b274732e01f11ccca503aaea13dfeff7fc39
Puneet Behl <behlp@objectcomputing.com>
2022-07-18 11:44:59 +0530

    Update to Grails 5.1.8 and Spring Boot 2.6.6 and changed default Micronaut version to 3.3.0
    Signed-off-by: Puneet Behl <behlp@objectcomputing.com>

--- src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
+++ src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
@@ -195,7 +195,7 @@ class GrailsGradlePlugin extends GroovyPlugin {
     }
 
     protected String getDefaultMicronautVersion() {
-        '3.2.7'
+        '3.3.0'
     }
 
     void addDefaultProfile(Project project, Configuration profileConfig) {
