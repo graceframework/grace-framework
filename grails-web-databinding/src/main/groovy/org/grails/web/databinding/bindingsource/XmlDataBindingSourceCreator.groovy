835a836e698158cd02b70d7ae14b9ef549e230c3
Michael Yan <rain@rainboyan.com>
2022-08-05 15:48:02 +0800

    Groovy 3: make tests passed, related commit https://github.com/rainboyan/grails-core/commit/36fd7886273010d1823a97580eb9d7ba9e8acda7

--- grails-web-databinding/src/main/groovy/org/grails/web/databinding/bindingsource/XmlDataBindingSourceCreator.groovy
+++ grails-web-databinding/src/main/groovy/org/grails/web/databinding/bindingsource/XmlDataBindingSourceCreator.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2013-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import groovy.transform.CompileStatic
 import groovy.xml.slurpersupport.GPathResult
 import grails.web.mime.MimeType
 
+import org.grails.databinding.bindingsource.DataBindingSourceCreator
 import org.grails.databinding.bindingsource.DataBindingSourceCreationException
 import org.grails.databinding.xml.GPathResultCollectionDataBindingSource
 import org.grails.databinding.xml.GPathResultMap
