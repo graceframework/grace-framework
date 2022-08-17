2dce28e6efda10d87dbfb778ff57868ce529bb2d
Michael Yan <rain@rainboyan.com>
2022-08-17 23:45:55 +0800

    Apply Codenarc UnnecessaryPublicModifier Rules
    Fix Codenarc UnnecessaryPublicModifier rules in grails-spring-boot main groovy source, see https://codenarc.org/codenarc-rules-unnecessary.html

--- grails-spring-boot/src/main/groovy/org/grails/spring/context/ApplicationContextExtension.groovy
+++ grails-spring-boot/src/main/groovy/org/grails/spring/context/ApplicationContextExtension.groovy
@@ -34,7 +34,7 @@ class ApplicationContextExtension {
      * @param name The bean name
      * @return
      */
-    public static Object propertyMissing(ApplicationContext applicationContext, String name) {
+    static Object propertyMissing(ApplicationContext applicationContext, String name) {
         if (applicationContext.containsBean(name)) {
             return applicationContext.getBean(name)
         }
@@ -50,7 +50,7 @@ class ApplicationContextExtension {
      * @param name The bean name
      * @return A bean or null
      */
-    public static Object getAt(ApplicationContext applicationContext, String name) {
+    static Object getAt(ApplicationContext applicationContext, String name) {
         return propertyMissing(applicationContext, name)
     }
 
