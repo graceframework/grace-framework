a770ebce4653f726cd508a6b005084f784bd122c
Michael Yan <rain@rainboyan.com>
2022-08-19 18:25:13 +0800

    Apply Codenarc UnnecessaryGString Rules
    Fix Codenarc UnnecessaryGString rules in grails-plugin-mimetypes main groovy source, see https://codenarc.org/codenarc-rules-unnecessary.html

--- grails-plugin-mimetypes/src/main/groovy/org/grails/web/mime/HttpServletResponseExtension.groovy
+++ grails-plugin-mimetypes/src/main/groovy/org/grails/web/mime/HttpServletResponseExtension.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 original authors
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -252,7 +252,7 @@ class HttpServletResponseExtension {
             boolean disabledForUserAgent = !(useAcceptHeaderXhr && request.xhr) && disableForUserAgents != null &&
                     userAgent ? disableForUserAgents.matcher(userAgent).find() : false
             if (msie) {
-                header = "*/*"
+                header = '*/*'
             }
             if (!header && useAcceptHeader && !disabledForUserAgent) {
                 header = request.getHeader(HttpHeaders.ACCEPT)
