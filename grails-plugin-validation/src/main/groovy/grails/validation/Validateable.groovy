3e019fd1555aca323826a47bdfffa1dd2537ce6a
Michael Yan <rain@rainboyan.com>
2022-08-19 18:26:58 +0800

    Apply Codenarc UnnecessaryGString Rules
    Fix Codenarc UnnecessaryGString rules in grails-plugin-validation main groovy source, see https://codenarc.org/codenarc-rules-unnecessary.html

--- grails-plugin-validation/src/main/groovy/grails/validation/Validateable.groovy
+++ grails-plugin-validation/src/main/groovy/grails/validation/Validateable.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -236,7 +236,7 @@ trait Validateable {
                     ConstraintEvalUtils.getDefaultConstraints(Holders.grailsApplication.config) : Collections.<String, Object>emptyMap()
             return new DefaultConstraintEvaluator(
                     new DefaultConstraintRegistry(messageSource),
-                    new KeyValueMappingContext(""),
+                    new KeyValueMappingContext(''),
                     defaultConstraints
             )
         }
