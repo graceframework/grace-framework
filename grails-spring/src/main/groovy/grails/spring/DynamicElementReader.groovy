a1c961fc46b922fae5493e5fa2dbab6b929a3925
Michael Yan <rain@rainboyan.com>
2022-08-19 18:27:57 +0800

    Apply Codenarc UnnecessaryGString Rules
    Fix Codenarc UnnecessaryGString rules in grails-spring main groovy source, see https://codenarc.org/codenarc-rules-unnecessary.html

--- grails-spring/src/main/groovy/grails/spring/DynamicElementReader.groovy
+++ grails-spring/src/main/groovy/grails/spring/DynamicElementReader.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2004-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,7 +84,7 @@ class DynamicElementReader extends GroovyObjectSupport {
             invokeAfterInterceptor = true
             firstCall = false
         }
-        if (name.equals("doCall")) {
+        if (name == 'doCall') {
             def callable = args[0]
             callable.resolveStrategy = Closure.DELEGATE_FIRST
             callable.delegate = this
