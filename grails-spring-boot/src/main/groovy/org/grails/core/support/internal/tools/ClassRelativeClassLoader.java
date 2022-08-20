00f056d4d6432938e862a8a5b6f330104164baff
Michael Yan <rain@rainboyan.com>
2022-08-21 01:10:25 +0800

    Apply Checkstyle Checks: Headers
    Checks that a source file begins with a specified header.
    
    Update all java source files with APL v2

--- grails-spring-boot/src/main/groovy/org/grails/core/support/internal/tools/ClassRelativeClassLoader.java
+++ grails-spring-boot/src/main/groovy/org/grails/core/support/internal/tools/ClassRelativeClassLoader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.grails.core.support.internal.tools;
 
 import grails.io.IOUtils;
@@ -20,6 +35,7 @@ import java.util.Enumeration;
  * @since 3.1.13
  */
 class ClassRelativeClassLoader extends URLClassLoader {
+
     public ClassRelativeClassLoader(Class targetClass) {
         super(createClassLoaderUrls(targetClass), ClassLoader.getSystemClassLoader());
     }
