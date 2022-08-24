7ea2b98823fccaecdb86f123f29862e55071ee0c
Puneet Behl <behlp@objectcomputing.com>
2022-08-24 23:45:49 +0530

    Improvements to Grails FindMainClassTask (#142)
    * Improve registerMainClassTask implementation (#133)
    
    Signed-off-by: Puneet Behl <behlp@objectcomputing.com>
    
    * Update GrailsGradlePlugin.groovy
    
    * Cleanup registerFindMainClassTask
    
    Signed-off-by: Puneet Behl <behlp@objectcomputing.com>

--- src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
+++ src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
@@ -502,13 +502,8 @@ class GrailsGradlePlugin extends GroovyPlugin {
             findMainClassTask.mustRunAfter(project.tasks.withType(GroovyCompile))
         } else if (!FindMainClassTask.class.isAssignableFrom(findMainClassTask.class)) {
             def grailsFindMainClass = project.tasks.register("grailsFindMainClass", FindMainClassTask).get()
-            grailsFindMainClass.mustRunAfter(findMainClassTask)
-            findMainClassTask.dependsOn(grailsFindMainClass)
-        }
-
-        def bootRepackageTask = project.tasks.findByName("bootRepackage")
-        if (bootRepackageTask) {
-            bootRepackageTask.dependsOn findMainClassTask
+            grailsFindMainClass.dependsOn(findMainClassTask)
+            findMainClassTask.finalizedBy(grailsFindMainClass)
         }
     }
 
