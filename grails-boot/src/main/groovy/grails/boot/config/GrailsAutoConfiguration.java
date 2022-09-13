551f7c64d83f533c22bdcb5cfd4fe20d3257ee4f
Michael Yan <rain@rainboyan.com>
2022-09-13 21:21:54 +0800

    Rename GrailsConfiguration to GrailsAutoConfiguration
    Remove GrailsAutoConfiguration.groovy

--- grails-boot/src/main/groovy/grails/boot/config/GrailsAutoConfiguration.java
+++ grails-boot/src/main/groovy/grails/boot/config/GrailsAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2015-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,8 +36,8 @@ import org.springframework.lang.Nullable;
  */
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureOrder(10000)
-@Import(GrailsConfiguration.GrailsRegistrar.class)
-public class GrailsConfiguration {
+@Import(GrailsAutoConfiguration.GrailsRegistrar.class)
+public class GrailsAutoConfiguration {
 
     static class GrailsRegistrar implements ImportBeanDefinitionRegistrar {
 
