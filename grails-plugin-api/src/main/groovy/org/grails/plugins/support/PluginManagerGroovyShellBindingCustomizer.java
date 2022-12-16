4a9b80ad0a089761d0bc83f20800f2fe9114806a
Michael Yan <rain@rainboyan.com>
2022-12-17 02:02:38 +0800

    Grails Plugin API: adding the new variable 'pluginManager' for Groovysh

---
+++ grails-plugin-api/src/main/groovy/org/grails/plugins/support/PluginManagerGroovyShellBindingCustomizer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.plugins.support;
+
+import groovy.lang.Binding;
+
+import grails.binding.GroovyShellBindingCustomizer;
+import grails.plugins.GrailsPluginManager;
+
+/**
+ * Grails PluginManager for the {@link GroovyShellBindingCustomizer}, adding the new variable 'pluginManager'.
+ *
+ * @author Michael Yan
+ * @since 2022.0.0
+ */
+public class PluginManagerGroovyShellBindingCustomizer implements GroovyShellBindingCustomizer {
+
+    private final GrailsPluginManager pluginManager;
+
+    public PluginManagerGroovyShellBindingCustomizer(GrailsPluginManager pluginManager) {
+        this.pluginManager = pluginManager;
+    }
+
+    @Override
+    public void customize(Binding binding) {
+        binding.setVariable("pluginManager", this.pluginManager);
+    }
+
+}
