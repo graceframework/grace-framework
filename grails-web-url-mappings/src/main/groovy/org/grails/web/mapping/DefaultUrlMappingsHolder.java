5c78a0e0bb69601694ca0c38b12d2a5ae99a759a
Michael Yan <rain@rainboyan.com>
2023-02-01 12:07:32 +0800

    Grails Web Url Mappings: Fixes java compile warnings

--- grails-web-url-mappings/src/main/groovy/org/grails/web/mapping/DefaultUrlMappingsHolder.java
+++ grails-web-url-mappings/src/main/groovy/org/grails/web/mapping/DefaultUrlMappingsHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1026,6 +1026,7 @@ public class DefaultUrlMappingsHolder implements UrlMappings {
         // A map from a UrlMappingsListKey to a list of UrlMappingKeys
         private final Map<UrlMappingsListKey, List<UrlMappingKey>> lookup = new HashMap<>();
 
+        @SuppressWarnings("unchecked")
         public void put(UrlMappingsListKey key, UrlMappingKey mapping) {
             List<UrlMappingKey> mappingsList = this.lookup.computeIfAbsent(key, k -> new ArrayList<>());
 
--- grails-web-url-mappings/src/main/groovy/org/grails/web/mapping/UrlMappingUtils.java
+++ grails-web-url-mappings/src/main/groovy/org/grails/web/mapping/UrlMappingUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,7 @@ import org.grails.web.util.WebUtils;
  * @author Graeme Rocher
  * @since 2.4
  */
+@SuppressWarnings("unchecked")
 public final class UrlMappingUtils {
 
     private UrlMappingUtils() {
