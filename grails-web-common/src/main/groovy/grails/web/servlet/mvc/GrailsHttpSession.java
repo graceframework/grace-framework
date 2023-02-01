65a0f7b0b466f8520cf8f76a4fb7c193a5713469
Michael Yan <rain@rainboyan.com>
2023-02-01 12:07:06 +0800

    Grails Web Common: Fixes java compile warnings

--- grails-web-common/src/main/groovy/grails/web/servlet/mvc/GrailsHttpSession.java
+++ grails-web-common/src/main/groovy/grails/web/servlet/mvc/GrailsHttpSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,8 +50,7 @@ public class GrailsHttpSession implements HttpSession {
         }
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public Enumeration getAttributeNames() {
+    public Enumeration<String> getAttributeNames() {
         createSessionIfNecessary();
         synchronized (this) {
             return this.adaptee.getAttributeNames();
--- grails-web-common/src/main/groovy/org/grails/web/util/WebUtils.java
+++ grails-web-common/src/main/groovy/org/grails/web/util/WebUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -203,6 +203,7 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
     /**
      * @deprecated Does not take into account the url converter
      */
+    @Deprecated
     public static String addViewPrefix(String viewName) {
         GrailsWebRequest webRequest = GrailsWebRequest.lookup();
         return addViewPrefix(viewName, webRequest != null ? webRequest.getControllerName() : null);
