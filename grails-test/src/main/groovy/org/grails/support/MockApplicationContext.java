c865e94fd102e7358b14fef0444f0c2a70a82c5b
Michael Yan <rain@rainboyan.com>
2023-02-01 12:17:07 +0800

    Grails Test: Fixes java compile warnings

--- grails-test/src/main/groovy/org/grails/support/MockApplicationContext.java
+++ grails-test/src/main/groovy/org/grails/support/MockApplicationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,7 @@ import org.springframework.web.context.support.StandardServletEnvironment;
 
 import grails.util.GrailsStringUtils;
 
+@SuppressWarnings("unchecked")
 public class MockApplicationContext extends GroovyObjectSupport implements WebApplicationContext {
 
     Date startupDate = new Date();
