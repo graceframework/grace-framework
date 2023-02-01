d7fd9a956ee40588a34b5283b770418bf6631231
Michael Yan <rain@rainboyan.com>
2023-02-01 12:10:08 +0800

    Grails Plugin Mimetypes: Fixes java compile warnings

--- grails-plugin-mimetypes/src/main/groovy/org/grails/plugins/web/mime/MimeTypesConfiguration.java
+++ grails-plugin-mimetypes/src/main/groovy/org/grails/plugins/web/mime/MimeTypesConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,6 +106,7 @@ public class MimeTypesConfiguration {
         return new DefaultMimeTypeResolver();
     }
 
+    @SuppressWarnings("unchecked")
     protected Map<CharSequence, Object> getMimeConfig(Config config) {
         return config.getProperty(Settings.MIME_TYPES, Map.class);
     }
