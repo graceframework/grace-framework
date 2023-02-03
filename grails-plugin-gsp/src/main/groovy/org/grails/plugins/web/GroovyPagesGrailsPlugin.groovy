34c45fe1a08aeb938701f8112a40107f9cf7eb19
Michael Yan <rain@rainboyan.com>
2023-02-03 20:20:06 +0800

    remove profiling logs

--- grails-plugin-gsp/src/main/groovy/org/grails/plugins/web/GroovyPagesGrailsPlugin.groovy
+++ grails-plugin-gsp/src/main/groovy/org/grails/plugins/web/GroovyPagesGrailsPlugin.groovy
@@ -268,7 +268,6 @@ class GroovyPagesGrailsPlugin extends Plugin {
             }
 
             // Now go through tag libraries and configure them in Spring too. With AOP proxies and so on
-            def start = System.currentTimeMillis()
             def taglibs = application.getArtefacts(TagLibArtefactHandler.TYPE)
             for (taglib in taglibs) {
                 final tagLibClass = taglib.clazz
@@ -281,7 +280,6 @@ class GroovyPagesGrailsPlugin extends Plugin {
                     //bean.scope = 'request'
                 }
             }
-            log.info(String.format("Found %d TagLibs: initialization completed in %d ms", taglibs.size(), (System.currentTimeMillis() - start)))
 
             errorsViewStackTracePrinter(ErrorsViewStackTracePrinter, ref('grailsResourceLocator'))
             filteringCodecsByContentTypeSettings(FilteringCodecsByContentTypeSettings, application)
