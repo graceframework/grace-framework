bda98eef0328f8d9ee8e5c60d419744a2ec0f24a
Michael Yan <rain@rainboyan.com>
2023-02-04 12:14:02 +0800

    Fixes errors: Execution failed for task ':publishGuide'
    Detail errors: Cannot cast object 'org.gradle.api.internal.project.DefaultAntBuilder@47af330a' with class 'org.gradle.api.internal.project.DefaultAntBuilder' to class 'groovy.ant.AntBuilder'
    
    because Gradle still use groovy.util.AntBuilder in Project.getAnt()

--- grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
+++ grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  */
 package grails.doc
 
-import groovy.ant.AntBuilder
 import groovy.io.FileType
 import groovy.text.Template
 import org.apache.commons.logging.LogFactory
@@ -96,7 +95,8 @@ class DocPublisher {
     /**
      * The AntBuilder instance to use
      */
-    AntBuilder ant
+    @SuppressWarnings("deprecation")
+    groovy.util.AntBuilder ant
 
     /**
      * The language we're generating for (gets its own sub-directory). Defaults to ''
