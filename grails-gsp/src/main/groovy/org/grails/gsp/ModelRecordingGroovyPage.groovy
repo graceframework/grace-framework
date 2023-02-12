3b6035d12f67f8ba30d1f44cda36dc78b58c171f
Michael Yan <rain@rainboyan.com>
2023-02-12 19:05:12 +0800

    Minor improvements

--- grails-gsp/src/main/groovy/org/grails/gsp/ModelRecordingGroovyPage.groovy
+++ grails-gsp/src/main/groovy/org/grails/gsp/ModelRecordingGroovyPage.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -162,7 +162,7 @@ class ModelEntry {
                                                 controllerName: 'CharSequence']
 
     Map<String, String> model = Collections.synchronizedMap([:])
-    Set<String> taglibs = Collections.synchronizedSet([] as Set)
+    Set<String> taglibs = Collections.synchronizedSet(new HashSet<>())
     Set<String> defaultTagLibs = new HashSet(GroovyPageParser.DEFAULT_TAGLIB_NAMESPACES)
     int initialSize
 
