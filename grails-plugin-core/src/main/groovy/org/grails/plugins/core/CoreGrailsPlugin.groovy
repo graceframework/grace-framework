34b14eb6c5f60a5de3514d90028a4ee604e657a8
Michael Yan <rain@rainboyan.com>
2023-02-21 20:09:15 +0800

    Grails Plugin Core: keep code clean
    Remove 'final' keyword in method arguments and reference variables, and convert 'final' to type

--- grails-plugin-core/src/main/groovy/org/grails/plugins/core/CoreGrailsPlugin.groovy
+++ grails-plugin-core/src/main/groovy/org/grails/plugins/core/CoreGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,8 +105,8 @@ class CoreGrailsPlugin extends Plugin implements PriorityOrdered {
             }
 
             // add shutdown hook if not running in war deployed mode
-            final warDeployed = Environment.isWarDeployed()
-            final devMode = !warDeployed && environment == Environment.DEVELOPMENT
+            boolean warDeployed = Environment.isWarDeployed()
+            boolean devMode = !warDeployed && environment == Environment.DEVELOPMENT
             if (devMode && ClassUtils.isPresent('jline.Terminal', application.classLoader)) {
                 shutdownHook(DevelopmentShutdownHook)
             }
