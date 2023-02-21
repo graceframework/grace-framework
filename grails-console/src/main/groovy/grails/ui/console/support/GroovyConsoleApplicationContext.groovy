aa6587d3e36c7b108467668d71e62e5609c6f7e1
Michael Yan <rain@rainboyan.com>
2023-02-21 19:56:12 +0800

    Grails Console: keep code clean
    Remove 'final' keyword in method arguments and reference variables, and convert 'final' to type
    
    remove unused code

--- grails-console/src/main/groovy/grails/ui/console/support/GroovyConsoleApplicationContext.groovy
+++ grails-console/src/main/groovy/grails/ui/console/support/GroovyConsoleApplicationContext.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ class GroovyConsoleApplicationContext extends GenericApplicationContext {
         binding.setVariable('ctx', this)
         binding.setVariable(GrailsApplication.APPLICATION_ID, getBean(GrailsApplication))
 
-        final GroovyConsoleApplicationContext self = this
+        GroovyConsoleApplicationContext self = this
         groovy.console.ui.Console groovyConsole = new groovy.console.ui.Console(binding) {
 
             @Override
--- grails-console/src/main/groovy/grails/ui/shell/support/GroovyshApplicationContext.groovy
+++ grails-console/src/main/groovy/grails/ui/shell/support/GroovyshApplicationContext.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,8 +37,6 @@ class GroovyshApplicationContext extends GenericApplicationContext {
         Binding binding = new Binding()
         binding.setVariable('ctx', this)
         binding.setVariable(GrailsApplication.APPLICATION_ID, getBean(GrailsApplication))
-
-        final GroovyshWebApplicationContext self = this
 
         new Groovysh(binding, new IO()).run('')
     }
