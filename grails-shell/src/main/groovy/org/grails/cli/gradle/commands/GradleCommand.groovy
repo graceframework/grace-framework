02137ab9dd3670af89d55cde4beca77a6aee08c5
Michael Yan <rain@rainboyan.com>
2023-03-09 00:36:12 +0800

    Fixes type cast error in GradleCommand

--- grails-shell/src/main/groovy/org/grails/cli/gradle/commands/GradleCommand.groovy
+++ grails-shell/src/main/groovy/org/grails/cli/gradle/commands/GradleCommand.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,7 @@ class GradleCommand implements ProjectCommand, Completer, ProjectContextAware {
     private void initializeCompleter() {
         if (completer == null && projectContext) {
             readTasks = new ReadGradleTasks(projectContext)
-            completer = new ClosureCompleter((Closure<Collection<String>>) { readTasks.call() })
+            completer = new ClosureCompleter((Closure<List<String>>) { readTasks.call() })
         }
     }
 
