7675640081d4b9e2960e05ce9a341e3d8ab90c3a
Michael Yan <rain@rainboyan.com>
2023-03-25 21:24:45 +0800

    Grails Shell: Update dependencies for GrailsCompilerAutoConfiguration

--- grails-shell/src/main/groovy/org/grails/cli/compiler/autoconfigure/GrailsCompilerAutoConfiguration.java
+++ grails-shell/src/main/groovy/org/grails/cli/compiler/autoconfigure/GrailsCompilerAutoConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.grails.cli.compiler.autoconfigure;
 
 import org.codehaus.groovy.ast.ClassNode;
@@ -6,6 +21,12 @@ import org.codehaus.groovy.control.customizers.ImportCustomizer;
 import org.grails.cli.compiler.CompilerAutoConfiguration;
 import org.grails.cli.compiler.DependencyCustomizer;
 
+/**
+ * {@link CompilerAutoConfiguration} for the Grails infrastructure.
+ *
+ * @author Michael Yan
+ * @since 2022.1.0
+ */
 public class GrailsCompilerAutoConfiguration extends CompilerAutoConfiguration {
 
     @Override
@@ -15,14 +36,18 @@ public class GrailsCompilerAutoConfiguration extends CompilerAutoConfiguration {
 
     @Override
     public void applyDependencies(DependencyCustomizer dependencies) {
-        dependencies.ifAnyMissingClasses("org.springframework.web.servlet.mvc.Controller").add("spring-boot-starter-web");
-        dependencies.ifAnyMissingClasses("grails.boot.config.GrailsAutoConfiguration").add("grails-boot");
-        dependencies.ifAnyMissingClasses("grails.core.DefaultGrailsApplication").add("grails-core");
+        dependencies.add("spring-boot-starter-actuator");
+        dependencies.add("spring-boot-starter-logging");
+        dependencies.add("spring-boot-starter-validation");
+        dependencies.add("spring-boot-starter-web");
+        dependencies.add("grails-boot");
+        dependencies.add("grails-core");
         dependencies.add("grails-web");
         dependencies.add("grails-plugin-i18n", "grails-plugin-codecs", "grails-plugin-controllers",
-                "grails-plugin-converters", "grails-plugin-databinding", "grails-plugin-interceptors", "grails-plugin-mimetypes",
-                "grails-plugin-rest", "grails-plugin-url-mappings");
-        dependencies.add("gsp", "plain", "jar", true);
+                "grails-plugin-converters", "grails-plugin-databinding", "grails-plugin-domain-class",
+                "grails-plugin-interceptors", "grails-plugin-mimetypes",
+                "grails-plugin-rest", "grails-plugin-services", "grails-plugin-url-mappings");
+        dependencies.add("fields", "gsp", "scaffolding");
     }
 
     @Override
