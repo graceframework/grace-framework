dae34f3ccd1e33f00dd7f297dac95d0f3044b0a7
Michael Yan <rain@rainboyan.com>
2023-04-04 15:43:59 +0800

    Micronaut: Remove supports for  Micronaut Spring

--- grails-boot/src/main/groovy/grails/boot/GrailsBuilder.java
+++ grails-boot/src/main/groovy/grails/boot/GrailsBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 the original author or authors.
+ * Copyright 2015-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,9 @@
  */
 package grails.boot;
 
-import io.micronaut.spring.context.MicronautApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.ClassUtils;
 
 /**
  * Fluent API for constructing Grails instances.
@@ -30,8 +28,6 @@ import org.springframework.util.ClassUtils;
  */
 public class GrailsBuilder extends SpringApplicationBuilder {
 
-    private MicronautApplicationContext micronautContext;
-
     public GrailsBuilder(Class<?>... sources) {
         super(sources);
     }
@@ -39,17 +35,6 @@ public class GrailsBuilder extends SpringApplicationBuilder {
     @Override
     protected SpringApplication createSpringApplication(ResourceLoader resourceLoader, Class<?>... sources) {
         return new Grails(resourceLoader, sources);
-    }
-
-    public GrailsBuilder enableMicronaut() {
-        if (ClassUtils.isPresent("io.micronaut.spring.context.MicronautApplicationContext", getClass().getClassLoader())) {
-            this.micronautContext = new MicronautApplicationContext();
-        }
-        else {
-            throw new IllegalStateException(
-                    "Class 'MicronautApplicationContext' not found (please add dependency 'micronaut-spring-context')");
-        }
-        return this;
     }
 
     @Override
@@ -63,14 +48,7 @@ public class GrailsBuilder extends SpringApplicationBuilder {
     }
 
     public Grails build(String... args) {
-        if (this.micronautContext != null) {
-            this.micronautContext.start();
-            super.parent(this.micronautContext);
-            return (Grails) super.build(args);
-        }
-        else {
-            return (Grails) super.build(args);
-        }
+        return (Grails) super.build(args);
     }
 
 }
