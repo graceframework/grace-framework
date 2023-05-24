42e56fc65ae634aadcba91bac6319e6aa98b3a83
Michael Yan <rain@rainboyan.com>
2023-05-24 10:26:34 +0800

    Grails Bootstrap: Update GrailsVersion in GrailsUtilTests

--- grails-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("2022.2.3-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2022.2.4-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
