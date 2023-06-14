5f12fbcda366d31f8cf8ae42b676ebd2c2cb3f1d
Michael Yan <rain@rainboyan.com>
2023-06-14 13:42:32 +0800

    Grails Core: Fixes DataTest as DomainClassArtefactHandler not implements GrailsApplicationAware

--- grails-test-support/src/main/groovy/grails/testing/gorm/DataTest.groovy
+++ grails-test-support/src/main/groovy/grails/testing/gorm/DataTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -155,7 +155,6 @@ trait DataTest extends GrailsUnitTest {
 
     private void initialMockDomainSetup() {
         ConstraintEvalUtils.clearDefaultConstraints()
-        ((DomainClassArtefactHandler) grailsApplication.getArtefactHandler(DomainClassArtefactHandler.TYPE)).setGrailsApplication(grailsApplication)
     }
 
     private void initializeMappingContext() {
