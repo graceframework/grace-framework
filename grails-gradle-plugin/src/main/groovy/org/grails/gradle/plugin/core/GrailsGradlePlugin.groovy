a17c0c1911a669253514d5a91e69e4fdc6641133
Michael Yan <rain@rainboyan.com>
2023-06-25 01:01:42 +0800

    Grails Gradle Plugin: Refine GrailsPluginGradlePlugin
    * Configure project name with the right GrailsPlugin

--- grails-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
+++ grails-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
@@ -746,11 +746,10 @@ class GrailsGradlePlugin extends GroovyPlugin {
         def configFile = project.file("$project.buildDir/config.groovy")
         configScriptTask.outputs.file(configFile)
 
-        def projectName = project.name
+        def projectName = getGrailsProjectName(project)
         def projectVersion = project.version
         def projectDir = project.projectDir.absolutePath
         def projectType = getGrailsProjectType()
-        def isPlugin = projectType == GrailsProjectType.PLUGIN
         def grailsAppDir = new File(project.projectDir, grailsAppDir).absolutePath
         configScriptTask.inputs.property('name', projectName)
         configScriptTask.inputs.property('version', projectVersion)
@@ -764,9 +763,6 @@ withConfig(configuration) {
         source.ast.putNodeMetaData('PROJECT_NAME', '$projectName')
         source.ast.putNodeMetaData('PROJECT_TYPE', '$projectType')
         source.ast.putNodeMetaData('PROJECT_VERSION', '$projectVersion')
-        classNode.putNodeMetaData('projectVersion', '$projectVersion')
-        classNode.putNodeMetaData('projectName', '$projectName')
-        classNode.putNodeMetaData('isPlugin', '$isPlugin')
     }
 }
 """
@@ -779,6 +775,10 @@ withConfig(configuration) {
 
     protected GrailsProjectType getGrailsProjectType() {
         GrailsProjectType.NONE
+    }
+
+    protected String getGrailsProjectName(Project project) {
+        return project.name
     }
 
     protected FileCollection buildClasspath(Project project, Configuration... configurations) {
