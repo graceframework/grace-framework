1a3fc993405b33feaf75720123aa0b0e2aaa076f
Michael Yan <rain@rainboyan.com>
2023-06-29 16:42:39 +0800

    Grails Core: Refine EnhancesTraitTransformation
    * Fixes not marked 'Generated' with the methods
    * Update tests

--- grails-core/src/test/groovy/grails/artefact/EnhancesSpec.groovy
+++ grails-core/src/test/groovy/grails/artefact/EnhancesSpec.groovy
@@ -1,11 +1,11 @@
 /*
- * Copyright 2015 original authors
+ * Copyright 2015-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,17 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package grails.artefact
 
 import grails.compiler.traits.TraitInjector
 import spock.lang.Specification
 
-
 /**
  * @author graemerocher
  */
-class EnhancesSpec extends Specification{
+class EnhancesSpec extends Specification {
     void "Test that the enhances trait transform works as expected"() {
 
         when:"The generated transformer is loaded"
