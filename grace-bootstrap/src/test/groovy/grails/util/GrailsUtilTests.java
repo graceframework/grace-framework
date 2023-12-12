5dadb61f3823ccd632b59c470d6c7763fa4a14b4
Michael Yan <rain@rainboyan.com>
2023-12-13 03:58:20 +0800

    Release v2021.0.0

--- grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("2021.0.0-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2021.0.0", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
