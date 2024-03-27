86abb4f904696b1dc8ea769e387ded95143c1c81
Michael Yan <rain@rainboyan.com>
2024-03-27 23:51:08 +0800

    Set default compatibility of ProfileCompilerTask to 17

--- grace-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/profiles/tasks/ProfileCompilerTask.groovy
+++ grace-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/profiles/tasks/ProfileCompilerTask.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 the original author or authors.
+ * Copyright 2015-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ import org.grails.gradle.plugin.profiles.GrailsProfileGradlePlugin
 @CompileStatic
 class ProfileCompilerTask extends AbstractCompile {
 
-    public static final String DEFAULT_COMPATIBILITY = '11'
+    public static final String DEFAULT_COMPATIBILITY = '17'
     public static final String PROFILE_NAME = 'name'
     public static final String PROFILE_COMMANDS = 'commands'
 
