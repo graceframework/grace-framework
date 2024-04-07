1dc01b7486c81c3f5eb98082fddd76b2c41c6e3b
Michael Yan <rain@rainboyan.com>
2024-04-07 10:48:54 +0800

    GrailsDevelopmentModeWatchApplicationContextInitializer compilerConfig.targetDirectory is not correct
    Closes gh-261

--- grace-boot/src/main/groovy/org/grails/boot/context/GrailsDevelopmentModeWatchApplicationContextInitializer.java
+++ grace-boot/src/main/groovy/org/grails/boot/context/GrailsDevelopmentModeWatchApplicationContextInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 the original author or authors.
+ * Copyright 2022-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -289,7 +289,7 @@ public class GrailsDevelopmentModeWatchApplicationContextInitializer implements
         File appDir = null;
         boolean sourceFileChanged = false;
         for (String dir : Arrays.asList(grailsAppPath, SOURCE_MAIN_JAVA, SOURCE_MAIN_GROOVY)) {
-            String changedDir = File.separator + dir;
+            String changedDir = new File(BuildSettings.BASE_DIR, dir).getAbsolutePath();
             if (changedPath.contains(changedDir)) {
                 appDir = new File(changedPath.substring(0, changedPath.indexOf(changedDir)));
                 sourceFileChanged = true;
