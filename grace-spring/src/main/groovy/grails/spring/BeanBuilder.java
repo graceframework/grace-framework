0886092572a4640b7accab5ca0351a56adccb0ba
Michael Yan <rain@rainboyan.com>
2024-04-09 01:11:41 +0800

    Explicitly set resolve strategy to `OWNER_FIRST` in `BeanBuilder.invokeBeanDefiningClosure`
    Closes gh-269

--- grace-spring/src/main/groovy/grails/spring/BeanBuilder.java
+++ grace-spring/src/main/groovy/grails/spring/BeanBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2023 the original author or authors.
+ * Copyright 2004-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -656,7 +656,7 @@ public class BeanBuilder extends GroovyObjectSupport {
      */
     protected BeanBuilder invokeBeanDefiningClosure(Closure<?> callable) {
         callable.setDelegate(this);
-//        callable.setResolveStrategy(Closure.DELEGATE_FIRST);
+        callable.setResolveStrategy(Closure.OWNER_FIRST);
         callable.call();
         finalizeDeferredProperties();
 
