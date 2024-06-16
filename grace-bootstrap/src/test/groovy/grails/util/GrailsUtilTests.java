4304285369e11c9c825259f5de7a3a9954041fac
Michael Yan <rain@rainboyan.com>
2024-06-16 23:00:51 +0800

    Release v2022.1.9

--- grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("2022.1.9-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2022.1.9", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
