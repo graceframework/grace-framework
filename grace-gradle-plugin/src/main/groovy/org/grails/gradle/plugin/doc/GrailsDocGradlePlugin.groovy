d35637570df413e1c540d976773473b7a14cc44b
Michael Yan <rain@rainboyan.com>
2024-06-17 15:22:35 +0800

    Migrate to `grace-docs` dependency in `GrailsDocGradlePlugin`
    Closes gh-374

--- grace-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/doc/GrailsDocGradlePlugin.groovy
+++ grace-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/doc/GrailsDocGradlePlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ class GrailsDocGradlePlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         Configuration docConfiguration = project.configurations.create(DOC_CONFIGURATION)
-        project.dependencies.add(DOC_CONFIGURATION, "org.grails:grails-docs:${BuildSettings.getPackage().getImplementationVersion()}")
+        project.dependencies.add(DOC_CONFIGURATION, "org.graceframework:grace-docs:${BuildSettings.getPackage().getImplementationVersion()}")
 
         Groovydoc groovydocTask = (Groovydoc) project.tasks.findByName('groovydoc')
         Javadoc javadocTask = (Javadoc) project.tasks.findByName('javadoc')
