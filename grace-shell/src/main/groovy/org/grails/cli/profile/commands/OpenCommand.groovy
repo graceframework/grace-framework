5616e9178401e418230728ef45cc2500912643e5
Michael Yan <rain@rainboyan.com>
2024-07-23 03:04:37 +0800

    Deprecate `open` command
    Closes gh-412

--- grace-shell/src/main/groovy/org/grails/cli/profile/commands/OpenCommand.groovy
+++ grace-shell/src/main/groovy/org/grails/cli/profile/commands/OpenCommand.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 the original author or authors.
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ import org.grails.cli.profile.ProjectCommand
 class OpenCommand implements ProjectCommand, Completer {
 
     public static final String NAME = 'open'
+    boolean deprecated = true
 
     @Override
     String getName() {
