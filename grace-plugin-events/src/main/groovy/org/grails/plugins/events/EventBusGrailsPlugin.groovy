768b234d383527105ee71aff2773f4cf56a7e7cb
Michael Yan <rain@rainboyan.com>
2024-10-15 13:50:16 +0800

    Auto-Configure Events Plugin
    Closes gh-5

--- grace-plugin-events/src/main/groovy/org/grails/plugins/events/EventBusGrailsPlugin.groovy
+++ grace-plugin-events/src/main/groovy/org/grails/plugins/events/EventBusGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 original authors
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,15 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.grails.plugins.events
 
-import grails.config.Config
 import grails.plugins.Plugin
-import groovy.util.logging.Slf4j
-import org.grails.events.bus.spring.EventBusFactoryBean
-import org.grails.events.gorm.GormDispatcherRegistrar
-import org.grails.events.spring.SpringEventTranslator
 
 /**
  * A plugin that integrates Reactor into Grails
@@ -29,27 +23,8 @@ import org.grails.events.spring.SpringEventTranslator
  * @author Graeme Rocher
  * @since 3.0
  */
-@Slf4j
 class EventBusGrailsPlugin extends Plugin {
 
     def grailsVersion = "2023.0.0 > *"
 
-    /**
-     * Whether to translate GORM events into reactor events
-     */
-    public static final String TRANSLATE_SPRING_EVENTS = "grails.events.spring"
-
-    @Override
-    Closure doWithSpring() {
-        {->
-            Config config = grailsApplication.config
-            grailsEventBus(EventBusFactoryBean)
-            gormDispatchEventRegistrar(GormDispatcherRegistrar, ref("grailsEventBus"))
-
-            // make it possible to disable reactor events
-            if(config.getProperty(TRANSLATE_SPRING_EVENTS, Boolean.class, false)) {
-                springEventTranslator(SpringEventTranslator, ref('grailsEventBus'))
-            }
-        }
-    }
 }
