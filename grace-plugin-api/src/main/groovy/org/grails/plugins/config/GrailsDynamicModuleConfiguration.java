2d283739e67d9ea76bd458726e9ef8371a0c1536
Michael Yan <rain@rainboyan.com>
2024-10-31 02:32:59 +0800

    Grace Plugin API: Generating Spring Boot configuration metadata files
    Closes gh-665

--- grace-plugin-api/src/main/groovy/org/grails/plugins/config/GrailsDynamicModuleConfiguration.java
+++ grace-plugin-api/src/main/groovy/org/grails/plugins/config/GrailsDynamicModuleConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 the original author or authors.
+ * Copyright 2021-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,11 +20,12 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Role;
+import org.springframework.core.Ordered;
 
 import org.grails.plugins.support.GrailsDynamicPluginRegistryPostProcessor;
 
 @AutoConfiguration
-@AutoConfigureOrder
+@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
 public class GrailsDynamicModuleConfiguration {
 
     @Bean
