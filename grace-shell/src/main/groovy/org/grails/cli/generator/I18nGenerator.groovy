49dfe193373870f4c5098d4b199c8647f12029c2
Michael Yan <rain@rainboyan.com>
2024-12-19 21:02:39 +0800

    Enhance `I18nGenerator`
    Add an option `lang`, you can choose which properties file to create or update,
    
    ```bash
    $ grace generate i18n --lang zh_CN
    ```
    
    Closes gh-814

--- grace-shell/src/main/groovy/org/grails/cli/generator/I18nGenerator.groovy
+++ grace-shell/src/main/groovy/org/grails/cli/generator/I18nGenerator.groovy
@@ -30,29 +30,42 @@ class I18nGenerator extends AbstractGenerator {
     @Override
     boolean generate() {
         String[] args = commandLine.remainingArgs.toArray(new String[0])
-        if (args.size() < 2) {
-            return
+
+        boolean overwrite = commandLine.hasOption('force') || commandLine.hasOption('f')
+        String lang = commandLine.optionValue('lang')
+
+        StringBuilder messages = new StringBuilder()
+        if (args.size() > 1) {
+            String className = args[1].capitalize()
+            String propertyName = className.uncapitalize()
+            Map<String, String> classAttributes = new LinkedHashMap<>()
+            String[] attributes = (args.size() >= 3 ? args[2..-1] : []) as String[]
+            attributes.each { String item ->
+                String[] attr = (item.contains(':') ? item.split(':') : [item, 'String']) as String[]
+                classAttributes[attr[0]] = attr[1]
+            }
+
+            messages.append('\n# ' + className + '\n')
+            messages.append(propertyName + '.label=' + className + '\n')
+            classAttributes.each { attr ->
+                String label = propertyName + '.' + attr.key + '.label'
+                String value = GrailsNameUtils.getNaturalName(attr.key)
+
+                messages.append(label + '=' + value + '\n')
+            }
         }
 
-        String className = args[1].capitalize()
-        String propertyName = className.uncapitalize()
-        Map<String, String> classAttributes = new LinkedHashMap<>()
-        String[] attributes = (args.size() >= 3 ? args[2..-1] : []) as String[]
-        attributes.each { String item ->
-            String[] attr = (item.contains(':') ? item.split(':') : [item, 'String']) as String[]
-            classAttributes[attr[0]] = attr[1]
+        String messagesFileName = 'app/i18n/messages' + (lang?.trim() ? '_' + lang?.trim() + '.properties' : '.properties')
+        File messagesFile = new File(getBaseDir(), messagesFileName)
+        if (messagesFile.exists() && !overwrite && messages.size() > 0) {
+            prependToFile(messagesFileName, messages.toString())
         }
-
-        StringBuilder text = new StringBuilder()
-        text.append('\n# ' + className + '\n')
-        classAttributes.each { attr ->
-            String label = propertyName + '.' + attr.key + '.label'
-            String value = GrailsNameUtils.getNaturalName(attr.key)
-
-            text.append(label + '=' + value + '\n')
+        else {
+            createFile('messages.properties.tpl', messagesFileName, [:], overwrite)
+            if (messages.size() > 0) {
+                prependToFile(messagesFileName, messages.toString())
+            }
         }
-
-        prependToFile('app/i18n/messages.properties', text.toString())
 
         true
     }
