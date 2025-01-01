f76a133aacd570bde379e8060734661e5f746021
Michael Yan <rain@rainboyan.com>
2025-01-01 10:48:46 +0800

    Next development version: 2023.1.2-SNAPSHOT

--- grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("2023.1.1-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2023.1.2-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
