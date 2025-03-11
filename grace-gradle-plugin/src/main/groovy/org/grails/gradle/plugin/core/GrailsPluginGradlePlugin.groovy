0fbd5185366caab57d564236b931499337eca812
Michael Yan <rain@rainboyan.com>
2025-03-11 22:47:51 +0800

    Exclude logback-spring.xml when packaging plugins
    Closes gh-948

--- grace-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/core/GrailsPluginGradlePlugin.groovy
+++ grace-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/core/GrailsPluginGradlePlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2024 the original author or authors.
+ * Copyright 2015-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -175,7 +175,7 @@ class GrailsPluginGradlePlugin extends GrailsGradlePlugin {
         project.tasks.named(JavaPlugin.JAR_TASK_NAME, Jar).configure { Jar jarTask ->
             jarTask.enabled = true
             jarTask.archiveClassifier.set('plugin')
-            jarTask.exclude('application.yml', 'application.groovy', 'logback.groovy', 'logback.xml')
+            jarTask.exclude('application.yml', 'application.groovy', 'logback.groovy', 'logback.xml', 'logback-spring.xml')
         }
     }
 
