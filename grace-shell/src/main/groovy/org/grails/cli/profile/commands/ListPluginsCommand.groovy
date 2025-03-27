403092c83b5835ff9220499fef267367e3226781
Michael Yan <rain@rainboyan.com>
2025-03-27 16:50:09 +0800

    Rewrite the Script Commands 'list-plugins' 'plugin-info' in Profile 'base' to Shell CLI Commands
    Closes gh-962

---
+++ grace-shell/src/main/groovy/org/grails/cli/profile/commands/ListPluginsCommand.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.cli.profile.commands
+
+import groovy.transform.CompileStatic
+
+import grails.build.logging.GrailsConsole
+import org.grails.cli.profile.Command
+import org.grails.cli.profile.CommandDescription
+import org.grails.cli.profile.ExecutionContext
+import org.grails.cli.profile.ProjectCommand
+
+/**
+ * Lists the available plugins from the Plugin Repository
+ *
+ * @author Michael Yan
+ * @since 2023.3.0
+ */
+@CompileStatic
+class ListPluginsCommand implements Command, ProjectCommand {
+
+    public static final String PLUGIN_REPO_URL = 'https://repo1.maven.org/maven2/org/graceframework/plugins'
+    private static final List<String> EXCLUDES = ['asset-pipeline-core', 'asset-pipeline-gradle', 'views-gradle']
+
+    final String name = 'list-plugins'
+    final CommandDescription description = new CommandDescription(name,
+            'Lists the available plugins from the Plugin Repository',
+            'grace list-plugins')
+
+
+    @Override
+    boolean handle(ExecutionContext executionContext) {
+        GrailsConsole console = executionContext.console
+        try {
+            console.addStatus 'Available Plugins'
+            String text = new URL(PLUGIN_REPO_URL).text
+            text.eachMatch(/<a href="([a-z-]+)\/" title="([a-z-]+)\/">.+/) { List<String> it ->
+                String plugin = it[1]
+                if (!EXCLUDES.contains(plugin)) {
+                    console.log "* ${it[1]}"
+                }
+            }
+
+            console.println()
+            console.log "You can find more information on https://central.sonatype.com/namespace/org.graceframework.plugins"
+        }
+        catch(Throwable e) {
+            console.error "Failed to list plugins", e
+            return false
+        }
+
+        true
+    }
+
+}
