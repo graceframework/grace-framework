10b8b69641dc5bdcaa3f691a5da83ed0479c68e4
Michael Yan <rain@rainboyan.com>
2025-04-02 16:45:45 +0800

    Move ScaffoldingBeanConfiguration from scaffolding-core to scaffolding plugin
    Closes gh-50

--- grace-plugin-scaffolding/src/main/groovy/org/grails/scaffolding/ScaffoldingBeanConfiguration.java
+++ grace-plugin-scaffolding/src/main/groovy/org/grails/scaffolding/ScaffoldingBeanConfiguration.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2022-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.grails.scaffolding;
 
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +41,12 @@ import org.grails.scaffolding.registry.DomainInputRendererRegistry;
 import org.grails.scaffolding.registry.DomainOutputRendererRegistry;
 import org.grails.scaffolding.registry.DomainRendererRegisterer;
 
+/**
+ * {@link EnableAutoConfiguration Auto-Configure} for Scaffolding
+ *
+ * @author Michael Yan
+ * @since 6.0
+ */
 @AutoConfiguration
 @AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
 public class ScaffoldingBeanConfiguration {
