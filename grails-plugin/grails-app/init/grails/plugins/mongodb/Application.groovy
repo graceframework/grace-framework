b7fd2d782c29e192f8ccf8e58856c0323b96c8a9
Michael Yan <rain@rainboyan.com>
2025-04-04 19:14:10 +0800

    Support Grace Framework and Grace Data 2023.3
    - Change Java baseline to 17
    - Update Gradle build scripts
    - Migrate examples to Grace apps
    - Fixes docs task
    - Upgrade to Grace Framework 2023.3.0-M1
    - Upgrade to Grace Data 2023.3.0-M1
    - Upgrade to Grace Asset Pipeline Plugin 6.3.0-M1
    - Upgrade to Groovy 4.0.24
    - Upgrade to Spock 2.3-groovy-4.0
    - Upgrade to Spring Framework 6.1.18
    - Upgrade to Spring Boot 3.3.7
    
    Closes gh-1

--- grails-plugin/grails-app/init/grails/plugins/mongodb/Application.groovy
+++ grails-plugin/grails-app/init/grails/plugins/mongodb/Application.groovy
@@ -1,15 +1,14 @@
 package grails.plugins.mongodb
 
-import grails.boot.GrailsApp
-import grails.boot.config.GrailsAutoConfiguration
+import grails.boot.Grails
 import grails.plugins.metadata.PluginSource
 import groovy.transform.CompileStatic
 
 @CompileStatic
 @PluginSource
-class Application extends GrailsAutoConfiguration {
+class Application {
 
     static void main(String[] args) {
-        GrailsApp.run((Class) Application, args)
+        Grails.run((Class) Application, args)
     }
 }
