205a7188c042ab07da89f7f316104dcf9cae88e8
Michael Yan <rain@rainboyan.com>
2025-09-19 19:58:21 +0800

    Refactor GrailsResourceBanner
    - Align with Spring Boot's `ResourceBanner`
    - Don't return the default application name in `Metadata.getApplicationName()`
    - Add GrailsResourceBannerTests

--- grace-boot/src/main/groovy/grails/boot/GrailsResourceBanner.java
+++ grace-boot/src/main/groovy/grails/boot/GrailsResourceBanner.java
@@ -16,11 +16,14 @@
 package grails.boot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.ResourceBanner;
+import org.springframework.boot.ansi.AnsiPropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
@@ -33,7 +36,7 @@ import grails.util.Metadata;
 
 /**
  * Banner implementation that prints from a source text {@link Resource}.
- * Add extra properties related Grails, such as Grails version, application name and version.
+ * Add extra properties related Grails, such as Grails version, application name, title and version.
  *
  * @author Michael Yan
  * @since 2022.0.0
@@ -47,48 +50,106 @@ public class GrailsResourceBanner extends ResourceBanner {
 
     @Override
     protected List<PropertyResolver> getPropertyResolvers(Environment environment, Class<?> sourceClass) {
-        List<PropertyResolver> resolvers = new ArrayList<>(super.getPropertyResolvers(environment, sourceClass));
-        resolvers.add(getGrailsVersionResolver(environment));
+        List<PropertyResolver> resolvers = new ArrayList<>();
+        resolvers.add(new PropertySourcesPropertyResolver(createNullDefaultSources(environment, sourceClass)));
+        resolvers.add(new PropertySourcesPropertyResolver(createEmptyDefaultSources(environment, sourceClass)));
         return resolvers;
     }
 
-    private PropertyResolver getGrailsVersionResolver(Environment environment) {
-        MutablePropertySources propertySources = new MutablePropertySources();
-        propertySources.addLast(new MapPropertySource("grace-name", getNamesMap(environment, "")));
-        propertySources.addLast(new MapPropertySource("grace-version", getVersionsMap(environment, "")));
-        return new PropertySourcesPropertyResolver(propertySources);
+    private MutablePropertySources createNullDefaultSources(Environment environment, Class<?> sourceClass) {
+        MutablePropertySources nullDefaultSources = new MutablePropertySources();
+        if (environment instanceof ConfigurableEnvironment configurableEnvironment) {
+            configurableEnvironment.getPropertySources().forEach(nullDefaultSources::addLast);
+        }
+        nullDefaultSources.addLast(getNameSource(sourceClass, environment, null));
+        nullDefaultSources.addLast(getTitleSource(sourceClass, environment, null));
+        nullDefaultSources.addLast(getAnsiSource());
+        nullDefaultSources.addLast(getVersionSource(sourceClass, environment, null));
+        return nullDefaultSources;
     }
 
-    private Map<String, Object> getNamesMap(Environment environment, String defaultValue) {
+    private MutablePropertySources createEmptyDefaultSources(Environment environment, Class<?> sourceClass) {
+        MutablePropertySources emptyDefaultSources = new MutablePropertySources();
+        emptyDefaultSources.addLast(getNameSource(sourceClass, environment, ""));
+        emptyDefaultSources.addLast(getTitleSource(sourceClass, environment, ""));
+        emptyDefaultSources.addLast(getVersionSource(sourceClass, environment, ""));
+        return emptyDefaultSources;
+    }
+
+    private MapPropertySource getNameSource(Class<?> sourceClass, Environment environment, String defaultValue) {
+        String applicationName = getApplicationName(sourceClass, environment);
+        Map<String, Object> nameMap = Collections.singletonMap("application.name",
+                (applicationName != null) ? applicationName : defaultValue);
+        return new MapPropertySource("name", nameMap);
+    }
+
+    protected String getApplicationName(Class<?> sourceClass, Environment environment) {
         String applicationName = environment.getProperty("info.app.name");
+        if (applicationName == null) {
+            applicationName = environment.getProperty("spring.application.name");
+        }
         if (applicationName == null) {
             applicationName = Metadata.getCurrent().getApplicationName();
         }
-        if (applicationName == null) {
-            applicationName = defaultValue;
-        }
-        Map<String, Object> names = new HashMap<>();
-        names.put("application.name", applicationName);
-        return names;
+        return applicationName;
     }
 
-    private Map<String, Object> getVersionsMap(Environment environment, String defaultValue) {
+    private MapPropertySource getTitleSource(Class<?> sourceClass, Environment environment, String defaultValue) {
+        String applicationTitle = getApplicationTitle(sourceClass);
+        if (applicationTitle == null) {
+            applicationTitle = getApplicationTitle(environment);
+        }
+        Map<String, Object> titleMap = Collections.singletonMap("application.title",
+                (applicationTitle != null) ? applicationTitle : defaultValue);
+        return new MapPropertySource("title", titleMap);
+    }
+
+    protected String getApplicationTitle(Environment environment) {
+        return environment.getProperty("info.app.title");
+    }
+
+    private AnsiPropertySource getAnsiSource() {
+        return new AnsiPropertySource("ansi", true);
+    }
+
+    private MapPropertySource getVersionSource(Class<?> sourceClass, Environment environment, String defaultValue) {
+        return new MapPropertySource("version", getVersionsMap(sourceClass, environment, defaultValue));
+    }
+
+    private Map<String, Object> getVersionsMap(Class<?> sourceClass, Environment environment, String defaultValue) {
         String appVersion = getApplicationVersion(environment);
         Map<String, Object> versions = new HashMap<>();
-        String grailsVersion = GrailsUtil.getGrailsVersion();
+        String bootVersion = getBootVersion();
+        String grailsVersion = getGrailsVersion();
         versions.put("application.version", getVersionString(appVersion, false, defaultValue));
         versions.put("grace.version", getVersionString(grailsVersion, false, defaultValue));
+        versions.put("spring-boot.version", getVersionString(bootVersion, false, defaultValue));
         versions.put("application.formatted-version", getVersionString(appVersion, true, defaultValue));
         versions.put("grace.formatted-version", getVersionString(grailsVersion, true, defaultValue));
+        versions.put("spring-boot.formatted-version", getVersionString(bootVersion, true, defaultValue));
         return versions;
+    }
+
+    @SuppressWarnings("removal")
+    @Deprecated(since = "2024.0.0", forRemoval = true)
+    protected String getApplicationVersion(Class<?> sourceClass) {
+        String applicationVersion = Metadata.getCurrent().getApplicationVersion();
+        return (applicationVersion != null) ? applicationVersion : super.getApplicationVersion(sourceClass);
     }
 
     private String getApplicationVersion(Environment environment) {
         String appVersion = environment.getProperty("info.app.version");
         if (appVersion == null) {
+            appVersion = environment.getProperty("spring.application.version");
+        }
+        if (appVersion == null) {
             appVersion = Metadata.getCurrent().getApplicationVersion();
         }
         return appVersion;
+    }
+
+    protected String getGrailsVersion() {
+        return GrailsUtil.getGrailsVersion();
     }
 
     private String getVersionString(String version, boolean format, String fallback) {
