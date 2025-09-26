74555e59231cda7e6748b7e41ef328336f74c736
Michael Yan <rain@rainboyan.com>
2025-09-26 23:59:46 +0800

    Add `EntityTraitInjector` and `GormTransformer` from `grace-datastore-gorm-support`
    See graceframework/grace-data#73

---
+++ grace-plugin-domain-class/src/main/groovy/org/grails/compiler/gorm/GormTransformer.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2011-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.compiler.gorm;
+
+import java.util.Collection;
+
+import groovy.transform.Canonical;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.classgen.GeneratorContext;
+import org.codehaus.groovy.control.SourceUnit;
+
+import grails.compiler.ast.AstTransformer;
+import grails.compiler.ast.GrailsArtefactClassInjector;
+
+import org.grails.compiler.injection.GrailsASTUtils;
+import org.grails.core.artefact.DomainClassArtefactHandler;
+import org.grails.datastore.mapping.reflect.AstUtils;
+
+/**
+ * Transforms GORM entities making the GORM API available to Java.
+ *
+ * @author Graeme Rocher
+ * @author Michael Yan
+ * @since 2.0
+ */
+@AstTransformer
+public class GormTransformer implements GrailsArtefactClassInjector {
+
+    @Override
+    public String[] getArtefactTypes() {
+        return new String[] { DomainClassArtefactHandler.TYPE };
+    }
+
+    public static Collection<String> getKnownEntityNames() {
+        return AstUtils.getKnownEntityNames();
+    }
+
+    @Override
+    public void performInjection(SourceUnit source, GeneratorContext context, ClassNode classNode) {
+        if (GrailsASTUtils.hasAnnotation(classNode, Canonical.class)) {
+            GrailsASTUtils.error(source, classNode, "Class [" + classNode.getName() + "] is marked with @groovy.transform.Canonical which is not supported for GORM entities.", true);
+        }
+        final GormEntityTransformation transformation = new GormEntityTransformation();
+
+        transformation.visit(classNode, source);
+    }
+
+    @Override
+    public void performInjection(SourceUnit source, ClassNode classNode) {
+        if (GrailsASTUtils.hasAnnotation(classNode, Canonical.class)) {
+            GrailsASTUtils.error(source, classNode, "Class [" + classNode.getName() + "] is marked with @groovy.transform.Canonical which is not supported for GORM entities.", true);
+        }
+        new GormEntityTransformation().visit(classNode, source);
+    }
+
+    @Override
+    public void performInjectionOnAnnotatedClass(SourceUnit source, ClassNode classNode) {
+        performInjection(source, classNode);
+    }
+
+}
