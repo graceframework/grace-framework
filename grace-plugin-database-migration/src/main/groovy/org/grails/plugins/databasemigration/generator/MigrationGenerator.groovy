fb85fff3e9af631b5e1251bd43de93ef09388d0c
Michael Yan <rain@rainboyan.com>
2026-01-29 00:19:23 +0800

    Fix missing return false

--- grace-plugin-database-migration/src/main/groovy/org/grails/plugins/databasemigration/generator/MigrationGenerator.groovy
+++ grace-plugin-database-migration/src/main/groovy/org/grails/plugins/databasemigration/generator/MigrationGenerator.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2025 the original author or authors.
+ * Copyright 2022-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ class MigrationGenerator extends AbstractGenerator {
     boolean generate() {
         String[] args = commandLine.remainingArgs.toArray(new String[0])
         if (args.size() < 2) {
-            return
+            return false
         }
 
         boolean overwrite = commandLine.hasOption('force') || commandLine.hasOption('f')
