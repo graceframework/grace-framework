008cdeefdb5dbf3584d48ac3ae09c4509052d292
Michael Yan <rain@rainboyan.com>
2026-01-29 23:51:39 +0800

    Enhance `MigrationGenerator`
    - Provide various ways to create migrations based on conventions and additional arguments.

--- grace-plugin-database-migration/src/main/groovy/org/grails/plugins/databasemigration/generator/MigrationGenerator.groovy
+++ grace-plugin-database-migration/src/main/groovy/org/grails/plugins/databasemigration/generator/MigrationGenerator.groovy
@@ -18,7 +18,10 @@ package org.grails.plugins.databasemigration.generator
 import java.time.ZonedDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 
 import grails.cli.generator.AbstractGenerator
@@ -31,10 +34,34 @@ import grails.util.GrailsNameUtils
 @CompileStatic
 class MigrationGenerator extends AbstractGenerator {
 
+    private static final Pattern ADD = Pattern.compile("^(add)_.*_to_(.*)")
+    private static final Pattern CREATE = Pattern.compile("^(create)_(.+)")
+    private static final Pattern REMOVE = Pattern.compile("^(remove)_.*?_from_(.*)")
+    private static final Pattern JOIN = Pattern.compile(".*_(join)_table_(.*)")
+    private static final Map<String, String> DATA_TYPES = [
+            'string': 'varchar(50)',
+            'String': 'varchar(50)',
+            'int': 'int',
+            'integer': 'int',
+            'Integer': 'int',
+            'long': 'bigint',
+            'Long': 'bigint',
+            'date': 'datetime',
+            'boolean': 'boolean',
+            'Boolean': 'boolean'
+    ]
+
     @Override
     boolean generate() {
         String[] args = commandLine.remainingArgs.toArray(new String[0])
         if (args.size() < 2) {
+            console.error('Missing the name of migration file!')
+            return false
+        }
+
+        String filename = args[1]
+        if (!(filename ==~ /^[_a-z0-9]+$/)) {
+            console.error("Illegal name for migration file: $filename.")
             return false
         }
 
@@ -42,13 +69,32 @@ class MigrationGenerator extends AbstractGenerator {
 
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of('UTC'))
         String migrationNumber = now.format(DateTimeFormatter.ofPattern('yyyyMMddHHmmss'))
-        String migrationName = GrailsNameUtils.getSnakeCaseName(GrailsNameUtils.getClassNameRepresentation(args[1]))
+        String migrationName = GrailsNameUtils.getSnakeCaseName(GrailsNameUtils.getClassNameRepresentation(filename))
         String migrationFileName = [migrationNumber, migrationName].join('_') + '.groovy'
 
         File changelogFile = new File(baseDir, 'db/migrations/changelog.groovy')
         Map<String, Object> model = new HashMap<>()
         model.put('id', System.currentTimeMillis().toString())
         model.put('author', getAuthor())
+        List matches = getTableAndActionName(migrationName)
+        if (matches) {
+            String tableName = matches[1]
+            String migrationAction = matches[0]
+            model.put('tableName', tableName)
+            model.put('migrationAction', migrationAction)
+            if (migrationAction == 'join') {
+                model.put('joinTables', args.size() > 3 ? args[2..-1] : tableName.toLowerCase().split('_'))
+            }
+            else {
+                Map<String, String> tableColumns = new LinkedHashMap<>()
+                String[] columns = (args.size() >= 3 ? args[2..-1] : []) as String[]
+                columns.each { String item ->
+                    String[] attr = (item.contains(':') ? item.split(':') : [item, 'string']) as String[]
+                    tableColumns[attr[0]] = attr[1]
+                }
+                model['tableColumns'] = tableColumns
+            }
+        }
 
         String migrationFile = 'db/migrations/' + migrationFileName
         createFile('Migration.groovy.tpl', migrationFile, model, overwrite)
@@ -64,6 +110,17 @@ class MigrationGenerator extends AbstractGenerator {
 
     private String getAuthor() {
         loadApplicationConfig().getProperty('dataSource.username') ?: System.getProperty('user.name')
+    }
+
+    @CompileDynamic
+    private List getTableAndActionName(String filename) {
+        for (Pattern it in [ADD, REMOVE, JOIN, CREATE]) {
+            Matcher matcher = it.matcher(filename)
+            if (matcher.matches()) {
+                return matcher[0][1..-1]
+            }
+        }
+        []
     }
 
 }
--- grace-plugin-database-migration/src/main/resources/META-INF/templates/generators/migration/Migration.groovy.tpl
+++ grace-plugin-database-migration/src/main/resources/META-INF/templates/generators/migration/Migration.groovy.tpl
@@ -1,7 +1,25 @@
 databaseChangeLog = {
 
     changeSet(author: "$author", id: "$id-1") {
-
+        <% if (migrationAction == 'create') { %>createTable(tableName: "$tableName") {
+<% tableColumns.each { name, type -> %>
+            column(name: "$name", type: "$type")
+<% } %>
+        }<% } else if (migrationAction == 'add') { %>addColumn(tableName: "$tableName") {
+<% tableColumns.each { name, type -> %>
+            column(name: "$name", type: "$type")
+<% } %>
+        }<% } else if (migrationAction == 'remove') { %>dropColumn(tableName: "$tableName") {
+<% tableColumns.each { name, type -> %>
+            column(name: "$name")
+<% } %>
+        }<% } else if (migrationAction == 'join') { %>createTable(tableName: "$tableName") {
+<% joinTables.each { t -> %>
+            column(name: "${t}_id", type: "BIGINT") {
+                constraints(referencedTableName: "$t", referencedColumnNames: "id", foreignKeyName: "FK_${t}_id", nullable: "false")
+            }
+<% } %>
+        }<% } %>
     }
 
 }
