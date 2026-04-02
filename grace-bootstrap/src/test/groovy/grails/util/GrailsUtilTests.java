e13924fdefb19a6d2cfbc3f92971e4ef01aea71f
Michael Yan <rain@rainboyan.com>
2026-04-02 12:42:42 +0800

    Next development version: 2024.0.1-SNAPSHOT

--- grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grace-bootstrap/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2025 the original author or authors.
+ * Copyright 2004-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,11 +30,12 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("2024.0.0-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2024.0.1-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
     protected void tearDown() throws Exception {
         System.setProperty(Environment.KEY, "");
     }
+
 }
