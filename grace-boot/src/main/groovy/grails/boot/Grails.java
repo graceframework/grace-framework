acde55e4935b54a8aee0c992b609fbf461b6f1bd
Michael Yan <rain@rainboyan.com>
2026-05-26 15:43:05 +0800

    Support setting system properties from the command line arguments
    Closes gh-1507

--- grace-boot/src/main/groovy/grails/boot/Grails.java
+++ grace-boot/src/main/groovy/grails/boot/Grails.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2025 the original author or authors.
+ * Copyright 2014-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package grails.boot;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.boot.SpringApplication;
@@ -29,6 +30,8 @@ import org.springframework.core.io.ResourceLoader;
 
 import grails.artefact.Artefact;
 import grails.util.Environment;
+import org.grails.build.parsing.CommandLine;
+import org.grails.build.parsing.CommandLineParser;
 
 /**
  * Extends the {@link SpringApplication} with reloading behavior and other Grails features
@@ -74,6 +77,13 @@ public class Grails extends SpringApplication {
 
     @Override
     protected void configureProfiles(ConfigurableEnvironment environment, String[] args) {
+        CommandLineParser parser = new CommandLineParser();
+        CommandLine commandLine = parser.parse(args);
+        for (Map.Entry<String, Object> option : commandLine.getUndeclaredOptions().entrySet()) {
+            if (option.getKey().startsWith("grails.")) {
+                System.setProperty(option.getKey(), option.getValue().toString());
+            }
+        }
         Environment env = Environment.getCurrent();
         environment.addActiveProfile(env.getName());
     }
