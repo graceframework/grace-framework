2aa95f97c4eba2ae4739356fc8b9d0a1c5881acb
Michael Yan <rain@rainboyan.com>
2026-05-31 18:10:43 +0800

    Add new method `invoke` to Generator
    Closes gh-1512

--- grace-cli/src/main/groovy/grails/cli/generator/Generator.java
+++ grace-cli/src/main/groovy/grails/cli/generator/Generator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 the original author or authors.
+ * Copyright 2022-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,10 @@ public interface Generator {
     }
 
     default void init(GenerationContext generationContext) {
+    }
+
+    default boolean invoke() {
+        return generate();
     }
 
     default boolean generate() {
